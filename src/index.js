e2e4c22667bef1b3a47a5d04425df48e28c44281
José <joseanania47@gmail.com>
2021-09-28 18:36:42 -0300

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import {saludar} from './js/componentes';
+import './styles.css';
+
+const nombre = 'José'
+saludar(nombre);
+
+// Producirá un error a menos que instalemos Webpack
---
+++ src/js/componentes.js
@@ -0,0 +1,20 @@
+// Componentes o Funciones
+
+import '../css/componentes.css';
+import webpacklogo from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre = 'Sin Nombre') => {
+    
+    console.log('Creando etiqueta h1');
+    
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola ${nombre}`;
+
+    document.body.append(h1);
+
+    //Imagen
+
+    const img = document.createElement('img');
+    img.src = webpacklogo;
+    document.body.append(img);
+}
